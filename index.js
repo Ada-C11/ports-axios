d998b66ae40bad8e381a8db704ee9a152edc7ff6
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-30 08:55:14 -0700

    Remove spoiler error handling

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
-const reportError = (message, errors) => {
-  let content = `<p>${message}</p><ul>`;
-  for (const field in errors) {
-    for (const problem of errors[field]) {
-      content += `<li>${field}: ${problem}</li>`;
-    }
-  }
-  content += "</ul>";
-  reportStatus(content);
-};
-
 //
 // Loading Pets
 //
