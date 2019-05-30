4b71afec015c8b305480ae62d0215ee043afac1a
Dan Roberts <dan@adadevelopersacademy.org>
2019-05-30 10:02:17 -0700

    Send a post request with hard-coded data

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const reportStatus = (message) => {
   $('#status-message').html(message);
 };
 
+const reportApiError = (error) => {
+  console.log("encountered error when posting", error);
+
+  let errorHtml = `<p>${error.message}</p><ul>`;
+
+  // some sort of loop nonsense
+  const fieldProblems = error.response.data.errors;
+
+  Object.keys(fieldProblems).forEach(field => {
+    const problems = fieldProblems[field];
+    problems.forEach(problem => {
+      errorHtml += `<li><strong>${field}:</strong> ${problem}</li>`;
+    });
+  });
+
+  errorHtml += '</ul>';
+  reportStatus(errorHtml);
+}
+
 //
 // Loading Pets
 //
@@ -29,10 +48,40 @@ const loadPets = () => {
     });
 };
 
+const readPetForm = () => {
+  return {
+    // name: "dan's ports pet don't use this name please I need it for debugging",
+    age: 14,
+    owner: "ports"
+  };
+}
+
+const handlePetFormSubmit = (event) => {
+  event.preventDefault();
+
+  const petData = readPetForm();
+
+  reportStatus("About to post pet data...");
+  console.log("About to post pet data", petData);
+
+  axios.post(URL, petData)
+    .then((response) => {
+      console.log("successfully posted pet data", response);
+
+      const petId = response.data.id;
+      reportStatus(`Successfully created a new pet with ID ${petId}`);
+    })
+    .catch((error) => {
+      reportApiError(error);
+    })
+};
+
 
 //
 // OK GO!!!!!
 //
 $(document).ready(() => {
   $('#load').click(loadPets);
+
+  $('#pet-form').submit(handlePetFormSubmit);
 });
